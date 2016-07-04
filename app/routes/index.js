530e3dbc3de08e2b0862e6eddea81d626d706ef4
Nerijus <nerijuskret@gmail.com>
2016-07-04 08:24:06 +0000

    initial commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,13 @@
-'use strict';
-
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
-
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
+"use strict";
+function createResponseObj(req){
+	var response = {"ipaddress": req.headers["x-forwarded-for"]};
+	response["language"] = req.headers["accept-language"].split(",")[0];
+	response["software"] = req.headers["user-agent"].split("(")[1].split(")")[0];
+	return response;
+}
+module.exports = function (app) {
+	app.route("/api/whoami")
 		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+			res.send(createResponseObj(req));
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
--- server.js
+++ server.js
@@ -2,13 +2,10 @@
 
 var express =require("express");
 var routes = require("./app/routes/index.js");
-var datesettings = {year: "numeric", month: "long", day: "numeric"};
 
 var app = express();
 
-app.use("/public", express.static(process.cwd() + "/public"));
-
-routes(app, datesettings);
+routes(app);
 
 app.listen(process.env["PORT"]);
 console.log("server is running on "+process.env["PORT"]);
